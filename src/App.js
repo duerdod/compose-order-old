b79ee42e80e92b8382ddb707ecc4d5ab7f848276
Erik Pehrers <erik.pehrers@jetshop.se>
2019-07-06 11:43:05 +0200

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span>hej</span>
+    </div>
+  );
+}
+
+export default App;
